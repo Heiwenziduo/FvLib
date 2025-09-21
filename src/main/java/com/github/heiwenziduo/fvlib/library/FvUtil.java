4d8b6599bcbb673243c2481debd52b5a254e1604
🐜 <125963716+Heiwenziduo@users.noreply.github.com>
2025-09-21 09:58:57 +0800

    isGenericMagic

--- src/main/java/com/github/heiwenziduo/fvlib/library/FvUtil.java
+++ src/main/java/com/github/heiwenziduo/fvlib/library/FvUtil.java
@@ -1,11 +1,15 @@
 package com.github.heiwenziduo.fvlib.library;
 
 import com.github.heiwenziduo.fvlib.api.mixin.LivingEntityMixinAPI;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+
+import static com.github.heiwenziduo.fvlib.data.FvLibDamageType.PURE;
 
 /**
  * <p>*welcum to TimeZone*</p>
- * <p><span style="color:#F28383">Waring:&nbsp;</span>1</p>
+ * <p><span style="color:f28383">Waring:&nbsp;</span>every class NOT in library folder will be changing destructively. Make sure you are using the right dependencies</p>
  */
 public class FvUtil {
     /// get time-lock ticks remain on an entity
@@ -16,5 +20,11 @@ public class FvUtil {
     /// set ticks to time-lock an entity
     public static void setTimeLock(LivingEntity living, int tick) {
         ((LivingEntityMixinAPI) living).FvLib$getTimeLockManager().setTimeLock(tick);
+    }
+
+    /// Damage Type: &nbsp;<span style="color: 47afff;">MAGIC</span>
+    public static boolean isGenericMagic(DamageSource damage) {
+        // every source not PURE and have BYPASSES_ARMOR tag will be defined as magic
+        return !damage.is(PURE) && !damage.is(DamageTypeTags.BYPASSES_ARMOR);
     }
 }
